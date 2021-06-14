024e65f3c50729a904d2f01a0e4cdfa8c0dab3d9
nadinehawat99 <67381733+nadinehawat99@users.noreply.github.com>
2021-06-14 21:07:58 +0300

    Update IHMFahrenheit.java

--- tp2/question3/IHMFahrenheit.java
+++ tp2/question3/IHMFahrenheit.java
@@ -6,44 +6,52 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class IHMFahrenheit extends JFrame implements ActionListener{
-  private JTextField entree = new JTextField( 6 );
-  /** Le bouton de conversion. */
-  private JButton  boutonDeConversion = new JButton( "convertir" );
-  /** La sortie en degrÈ Celsius. */
-  private JTextField sortie = new JTextField( 6 );
+    private JTextField entree = new JTextField( 6 );
+    /** Le bouton de conversion. */
+    private JButton  boutonDeConversion = new JButton( "convertir" );
+    /** La sortie en degr√© Celsius. */
+    private JTextField sortie = new JTextField( 6 );
 
-  public IHMFahrenheit(){
-    super("IHM Fahrenheit");
- 
-    setLayout(new FlowLayout());
-    add( entree ); add( boutonDeConversion ); add( sortie );
-    sortie.setEditable( false );
-    getContentPane().setBackground( Color.pink );
-    setLocation(100,100);
-    pack();setVisible(true);
-    
-    boutonDeConversion.addActionListener( this );
-  }
+    public IHMFahrenheit(){
+        super("IHM Fahrenheit");
 
-  /** 
-   * mÈthode dÈclenchÈe lorsque le bouton de conversion est appuyÈ. 
-   * remarquer que le champs de droite (les degrÈs Celsius) n'est pas Èditable.
-   * @param ae l'ÈvÈnement transmis
-   */
-  public void actionPerformed( ActionEvent ae ){
-    try{
-      int fahrenheit = 0; // valeur est une String et doit Ítre convertie en entier, voir java.lang.Integer mÈthode parseInt (--> try/catch)
-      float celsius = 0F; // ‡ complÈter, en appelant la mÈthode ad'hoc de la question2 
-      // un test ici pour le zÈro absolu (-273.1)
+        setLayout(new FlowLayout());
+        add( entree ); add( boutonDeConversion ); add( sortie );
+        sortie.setEditable( false );
+        getContentPane().setBackground( Color.pink );
+        setLocation(100,100);
+        pack();setVisible(true);
 
-      sortie.setText( Float.toString( celsius));
-    }catch(NumberFormatException nfe){
-      sortie.setText("error ! ");
+        boutonDeConversion.addActionListener( this );
     }
-  }
-  
-  
-  public static void main(String[] args){
-      new IHMFahrenheit();
+
+    /** 
+     * m√©thode d√©clench√©e lorsque le bouton de conversion est appuy√©. 
+     * remarquer que le champs de droite (les degr√©s Celsius) n'est pas √©ditable.
+     * @param ae l'√©v√©nement transmis
+     */
+    public void actionPerformed( ActionEvent ae ){
+        int fahrenheit = 0; 
+        double celsius = 0.0; 
+        double minValeur = -273.1;
+
+        try {
+            if(ae.getSource() == boutonDeConversion){
+                fahrenheit = Integer.parseInt(entree.getText());
+                celsius = question2.FahrenheitCelsius.fahrenheitEnCelsius(fahrenheit);
+
+                if (celsius < minValeur)
+                    celsius = minValeur;
+
+                sortie.setText(Double.toString(celsius));
+            }
+        } catch (NumberFormatException nfe){
+            sortie.setText("error ! ");
+        }
+
+    }
+
+    public static void main(String[] args){
+        new IHMFahrenheit();
     }
 }
